288dba7e749268bc2f3ca782a483d1dd348eb48d
John <namrods15@gmail.com>
2022-07-31 18:21:55 +0200

    First commit of experiementing with caesar cipher
    This is the first section which handles the encryption of a string, however
    future plans are to include symbols and numbers as well as decryption to be added.
    
    This will be a possible future feature for the password generator.

---
+++ cipher.py
@@ -0,0 +1,28 @@
+# This is the first step for the caesar cipher this section handles the encryption of a string.
+
+# To start with the program works for only string inputs.
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# Getting user input.
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+# Decryption function to encrypt string to given shift.
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    
+    # Loops through each letter that the use enters and changes it via the shift amount.
+    for letter in plain_text:
+        letter_index = alphabet.index(letter)
+        new_index = letter_index + shift_amount
+
+        if new_index > len(alphabet) -1:
+            new_index -= len(alphabet)
+
+        cipher_text += alphabet[new_index]
+
+    print(f"The encoded text is {cipher_text}")
+
+encrypt(text, shift)
