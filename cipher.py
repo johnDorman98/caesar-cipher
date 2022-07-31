36fdaf479a5fcfc6badcb55c1350272215d881a9
John <namrods15@gmail.com>
2022-07-31 18:43:28 +0200

    Added the README and also removed the option to enter in decode.

--- cipher.py
+++ cipher.py
@@ -5,7 +5,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # Getting user input.
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt your word.\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
